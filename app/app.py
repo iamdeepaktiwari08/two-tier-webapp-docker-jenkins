41807d2032f49f40ee213e00effe985c10d711bb
Deepak Tiwari <imdeepaktiwari08@gmail.com>
2025-12-07 16:17:59 +0530

    Update home route response message

--- app/app.py
+++ app/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Auto Deploy Test — Webhook Working!"
+    return "Auto Deploy Test 1 — Webhook Working!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
